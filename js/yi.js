60771947b487c82296eba6319e306e8a93dc7bc7
张家硕 <115350536+shuo-esvn@users.noreply.github.com>
2023-02-24 00:49:41 +0800

    Add files via upload

---
+++ js/yi.js
@@ -0,0 +1,14 @@
+var x = document.getElementById("demo");
+
+function getLocation() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(showPosition);
+	} else {
+		x.innerHTML = "该浏览器不支持获取地理位置。";
+	}
+}
+
+function showPosition(position) {
+	x.innerHTML = "纬度: " + position.coords.latitude +
+		"    经度: " + position.coords.longitude;
+}
